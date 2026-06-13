6028b5d304f6a77c5eb37182edb7da757a2c729d
Ghanmidev2012 <242857573+Ghanmidev2012@users.noreply.github.com>
2026-06-13 10:43:39 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Welcom to morocco</h1>
+     <br></br>
+     <h1>About morocco</h1>
+     <p className="marocpara">Morocco is a North african country bordering the atlantic ocean and Mediterranean Sea, distinguished by its Berber, Arabian, and European cultural influences.</p>
+     <br></br>
+     <h1>Cpital of morocco</h1>
+     <p className="marocpara">The capital morocco is Rabat</p>
+     <div className="marocimg">
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGlv5Gf-lw1SNx4PS16GmvEul-sIWfb5JJt02EELKtiDSkRiK8504nNNnQ&s=10" alt="hassan tour"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjx2KzSafXk8C8nBdVBBigSWfF--63LsCMbJ7hr2DH1g&s=10"></img>
+     </div>
+    </>
+  )
+}
+
+export default App
